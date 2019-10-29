c1c1d1c405cba200e1fd2f4cc45872e09882cb85
no-Seaweed <40819442+no-Seaweed@users.noreply.github.com>
2019-10-28 20:53:28 -0400

    Add files via upload

--- knn.py
+++ knn.py
@@ -1,13 +1,38 @@
-class KNeighborsClassifier:
+import numpy as np
 
+
+class KNeighborsClassifier:
     def __init__(self, k):
         self.k = k
 
-    def fit(self, X_train, y_train):
-        raise NotImplementedError()
+    # def fit(self, X_train, y_train):
+        # np.sqrt(np.sum(np.power(, 2)))
+        # raise NotImplementedError()
 
-    def predict(self, X_test):
-        raise NotImplementedError()
+    def predict(self, X_train, X_test, y_train):
+        # raise NotImplementedError()
+        train = np.concatenate((X_train, y_train.reshape(100, 1)), axis=1)
+        distance_store = []
+        neighbor = []
+        for i in range(X_test.shape[0]):
+            for j in range(X_train.shape[0]):
+                euclidean_distance = np.sqrt(np.sum((X_test[i, :]-X_train[j, :])**2))
+                distance_store.append(euclidean_distance)
+            concat_temp = np.concatenate((np.array(distance_store).reshape(100, 1), y_train.reshape(100, 1)), axis=1)
+            dist = concat_temp[concat_temp[:, 0].argsort()][:self.k, -1]
+            distance_store = []
+            neighbor.append(dist)
+            dist = []
+        # print(np.array(neighbor))
+        neighbors = np.array(neighbor)
+        # print(neighbors.shape)
+        y_pred = []
+        for neighbor in neighbors:
+            (values, counts) = np.unique(neighbor, return_counts=True)
+            ind = np.argmax(counts)
+            y_pred.append(int(values[ind]))
+        return np.array(y_pred)
 
-    def score(self, X_test, y_test):
-        raise NotImplementedError()
+    def score(self, y_pred_test, y_test):
+        # raise NotImplementedError()
+        return float(sum(y_pred_test == y_test)) / float(len(y_test))
--- main.py
+++ main.py
@@ -9,14 +9,13 @@ ynames = iris.target_names
 
 X, y = utils.shuffle(X, y, random_state=1)
 train_set_size = 100
-X_train = X[:train_set_size]  # selects first 100 rows (examples) for train set
-y_train = y[:train_set_size]
+X_train = X[:train_set_size]  # selects first 100 rows (examples) for train set 100,4
+y_train = y[:train_set_size]  # 100
 X_test = X[train_set_size:]   # selects from row 100 until the last one for test set
 y_test = y[train_set_size:]
-
 k = 5
 knn = KNeighborsClassifier(k=k)
 
-knn.fit(X_train, y_train)
-y_pred_test = knn.predict(X_test)
+# knn.fit(X_train, y_train)
+y_pred_test = knn.predict(X_train, X_test, y_train)
 print("Accuracy of KNN test set:", knn.score(y_pred_test, y_test))
